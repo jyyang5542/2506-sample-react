fcaceba47cae6548d039176d1a6086d3055f435a
유젠/양정연 <q93tiw@viashinsegae.com>
2025-08-13 16:21:49 +0900

    feat: AOS 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useAos } from './useAos';
 export { default as useBlurOnScroll } from './useBlurOnScroll';
 export { default as useHandleMediaError } from './useHandleMediaError';
 export { default as useObserveInView } from './useObserveInView';
---
+++ src/hooks/useAos.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
+const useAos = () => {
+	useEffect(() => {
+		AOS.init();
+	}, []);
+
+	return null;
+};
+
+export default useAos;
